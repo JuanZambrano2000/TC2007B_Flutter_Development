8a71371d14e7bd39fa47e511751ef391d9d7b8e6
Juan Zambrano <jpablozambrano@hotmail.com>
2023-09-25 08:03:06 -0600

    intro finished

--- flutter_1/lib/main.dart
+++ flutter_1/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
@@ -133,9 +135,12 @@ class _EjemploListaState extends State<EjemploLista> {
         Navigator.push(
             context,
             MaterialPageRoute(
+                /*
                 builder: (context) => DetailView(
                       externalArg: value,
                     )));
+              */
+                builder: (context) => const RequestDetail()));
       },
     );
   }
@@ -155,6 +160,53 @@ class DetailView extends StatelessWidget {
   }
 }
 
+//stf+tab to create a statefulWidget
+class RequestDetail extends StatefulWidget {
+  const RequestDetail({super.key});
+
+  @override
+  State<RequestDetail> createState() => _RequestDetailState();
+}
+
+class _RequestDetailState extends State<RequestDetail> {
+  late Future<List<Car>> cars;
+  @override
+  void initState() {
+    super.initState();
+    cars = getCars();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Detail with request")),
+      body: Center(
+        child: FutureBuilder<List<Car>>(
+            future: cars,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                // request is done and we hava data
+                List<Widget> result = [];
+                for(var currentCar in snapshot.data!){
+                  result.add(Text("${currentCar.brand} ${currentCar.model} ${currentCar.year}"))
+                }
+                return Column(
+                  children: [
+                    //! i don't care if its null or not bring it on
+                    Text(snapshot.data![0].brand),
+                    Text(snapshot.data![0].model),
+                    Text("${snapshot.data![0].year}"),
+                  ],
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              return const CircularProgressIndicator();
+            }),
+      ),
+    );
+  }
+}
 // let's do a request!
 // to do so we need to do 2 things -
 // 1. Define a class that will be used to parse a JSON
@@ -179,4 +231,18 @@ class Car {
 // We can declar a method with no class
 // there is no restriction whatsoever on dart!
 
-// Future<List<Car>> getCars() async {}
+Future<List<Car>> getCars() async {
+  final response = await http.get(Uri.parse(
+      "https://bitbucket.org/itesmguillermorivas/partial2/raw/45f22905941b70964102fce8caf882b51e988d23/carros.json"));
+  if (response.statusCode == 200) {
+    List<dynamic> jsonResult = jsonDecode(response.body);
+    List<Car> cars = [];
+    for (var current in jsonResult) {
+      Car currentCar = Car.fromJSON(current);
+      cars.add(currentCar);
+    }
+    return cars;
+  } else {
+    throw Exception("Request had errors");
+  }
+}
