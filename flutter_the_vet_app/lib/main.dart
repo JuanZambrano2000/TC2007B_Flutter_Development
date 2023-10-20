7e8295c5cd2fe6a7e5527fe52b7be7954871815c
Juan Zambrano <jpablozambrano@hotmail.com>
2023-10-20 00:20:43 -0600

    Update main.dart

--- flutter_the_vet_app/lib/main.dart
+++ flutter_the_vet_app/lib/main.dart
@@ -46,9 +46,6 @@ class _LoginWidgetState extends State<LoginWidget> {
   @override
   Widget build(BuildContext context) {
     var db = FirebaseFirestore.instance;
-    // how to check when a user is connected in realtime
-    // design pattern - singleton
-    // https://en.wikipedia.org/wiki/Singleton_pattern
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user != null) {
         print("USER: ${user.uid}");
@@ -56,7 +53,6 @@ class _LoginWidgetState extends State<LoginWidget> {
         print("SIGNED OUT");
       }
     });
-
     // lets add some states!
     TextEditingController login = TextEditingController();
     TextEditingController password = TextEditingController();
@@ -66,89 +62,184 @@ class _LoginWidgetState extends State<LoginWidget> {
       password.text = "";
     });
 
-    return Column(
-      children: [
-        Container(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Login',
-              ),
-              controller: login,
-            )),
-        Container(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-              controller: password,
-              obscureText: true,
-            )),
-        TextButton(
-            onPressed: () async {
-              try {
-                final user = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: login.text, password: password.text);
-                print("USER CREATED: ${user.user?.uid}");
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'weak-password') {
-                  print("your password is weak and so are you.");
-                } else if (e.code == 'email-already-in-use') {
-                  print("account exists.");
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+                controller: login,
+              )),
+          Container(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+                controller: password,
+                obscureText: true,
+              )),
+          TextButton(
+              onPressed: () async {
+                try {
+                  final user = await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                          email: login.text, password: password.text);
+                  print("USER CREATED: ${user.user?.uid}");
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    print("your password is weak and so are you.");
+                  } else if (e.code == 'email-already-in-use') {
+                    print("account exists.");
+                  }
+                } catch (e) {
+                  print(e);
                 }
-              } catch (e) {
-                print(e);
-              }
-            },
-            child: const Text("Sign up")),
-        TextButton(
-            onPressed: () async {
-              try {
-                final user = await FirebaseAuth.instance
-                    .signInWithEmailAndPassword(
-                        email: login.text, password: password.text);
-                print("USER LOGGED IN: ${user.user?.uid}");
-              } catch (e) {
-                print(e);
-              }
-            },
-            child: const Text("Log in")),
-        TextButton(
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-            },
-            child: const Text("Log out")),
-        TextButton(
-            onPressed: () async {
-              // create an object that will be translated into a document
-              // remember - firestore has a structure in which collections contain documents
-              // documents are similar to records in a relational db
-              final puppy = <String, dynamic>{
-                "name": "Killer",
-                "breed": "Chihuahueño",
-                "age": 1.0
-              };
-
-              // add user into firestore collection
-              db.collection("puppies").add(puppy).then(
-                  (DocumentReference doc) =>
-                      print("new document created: ${doc.id}"));
-            },
-            child: const Text("Add record")),
-        TextButton(
-            onPressed: () async {
-              await db.collection("puppies").get().then((value) {
-                for (var doc in value.docs) {
-                  print("DOCUMENT: ${doc.data()}");
+              },
+              child: const Text("Sign up")),
+          TextButton(
+              onPressed: () async {
+                try {
+                  final user = await FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                          email: login.text, password: password.text);
+                  if (user != null) {
+                    print("USER LOGGED IN: ${user.user?.uid}");
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MenuActivity()));
+                  }
+                } catch (e) {
+                  print(e);
                 }
-              });
-            },
-            child: const Text("Query")),
-      ],
+              },
+              child: const Text("Sign in")),
+        ],
+      ),
     );
+  }
+}
+
+class MenuActivity extends StatefulWidget {
+  @override
+  _MenuActivityState createState() => _MenuActivityState();
+}
+
+class _MenuActivityState extends State<MenuActivity> {
+  final db = FirebaseFirestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Menu Activity')),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: db.collection('animals').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(
+                child: Text(
+                    "There are no animals registered, register the first one"));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) =>
+                buildListItem(context, snapshot.data!.docs[index]),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => Navigator.push(context,
+              MaterialPageRoute(builder: (context) => DetailActivity()))),
+    );
+  }
+
+  Widget buildListItem(BuildContext context, DocumentSnapshot document) {
+    return ListTile(
+      title: Text(document['name']),
+      trailing: IconButton(
+          icon: const Icon(Icons.info),
+          onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      DetailActivity(animalDocument: document)))),
+    );
+  }
+}
+
+class DetailActivity extends StatefulWidget {
+  final DocumentSnapshot? animalDocument;
+
+  DetailActivity({this.animalDocument});
+
+  @override
+  _DetailActivityState createState() => _DetailActivityState();
+}
+
+class _DetailActivityState extends State<DetailActivity> {
+  final db = FirebaseFirestore.instance;
+
+  late TextEditingController nameController;
+  late TextEditingController ageController;
+  late TextEditingController weightController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    nameController =
+        TextEditingController(text: widget.animalDocument?['name'] ?? '');
+    ageController = TextEditingController(
+        text: widget.animalDocument?['age']?.toString() ?? '');
+    weightController = TextEditingController(
+        text: widget.animalDocument?['weight']?.toString() ?? '');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Detail Activity')),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+                controller: nameController,
+                decoration: InputDecoration(labelText: 'Name')),
+            TextField(
+                controller: ageController,
+                decoration: InputDecoration(labelText: 'Age')),
+            TextField(
+                controller: weightController,
+                decoration: InputDecoration(labelText: 'Weight')),
+            ElevatedButton(onPressed: registerAnimal, child: Text('Register'))
+          ],
+        ),
+      ),
+    );
+  }
+
+  void registerAnimal() {
+    final animal = {
+      'name': nameController.text,
+      'age': double.tryParse(ageController.text),
+      'weight': double.tryParse(weightController.text)
+    };
+
+    if (widget.animalDocument == null) {
+      db.collection('animals').add(animal);
+    } else {
+      db.collection('animals').doc(widget.animalDocument!.id).update(animal);
+    }
+
+    Navigator.pop(context);
   }
 }
